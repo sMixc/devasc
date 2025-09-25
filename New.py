a80aea20bb64019325be9c8dfb3d4996f3cb2262
Jaz4002 <wongjasmin23@gmail.com>
2025-09-25 17:18:38 +0800

    change title page

--- New.py
+++ New.py
@@ -5,7 +5,7 @@ import folium
 from streamlit_folium import st_folium
 
 # --- CONFIG ---
-st.set_page_config(page_title="Graphhopper Route Finder", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="RouteMatch", layout="wide", initial_sidebar_state="expanded")
 route_url = "https://graphhopper.com/api/1/route?"
 key = "571cae75-8241-46bb-af7f-62d0eaa4bc96" 
 
